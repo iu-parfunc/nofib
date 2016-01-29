4efadda2c11e96df44d04be17521051a7dff6f46
RyanGlScott <ryan.gl.scott@gmail.com>
2016-01-29 12:27:16 -0500

    Tweaks

---
+++ benchmark_nofib_StrictData.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -xe
+
+WORKDIR=`pwd`
+GHC_TOP_DIR="${HOME}"/opt/ghc-8.0.0-20160111
+GHC_BIN_DIR="${GHC_TOP_DIR}"/bin
+
+LOG_NORMAL=nofib-log-normal
+LOG_STRICT=nofib-log-strict
+
+COMP_NORMAL_STRICT=nofib-comp-normal-strict
+
+export PATH=${GHC_BIN_DIR}:$PATH
+
+( cd html-1.0.1.2/ \
+  && runghc Setup configure --prefix="${GHC_TOP_DIR}" \
+  && runghc Setup build \
+  && runghc Setup install )
+
+( cd mtl-2.2.1/ \
+  && runghc Setup configure --prefix="${GHC_TOP_DIR}" \
+  && runghc Setup build \
+  && runghc Setup install )
+
+( cd regex-base-0.93.2/ \
+  && runghc Setup configure --prefix="${GHC_TOP_DIR}" \
+  && runghc Setup build \
+  && runghc Setup install )
+
+( cd regex-posix-0.95.2/ \
+  && runghc Setup configure --prefix="${GHC_TOP_DIR}" \
+  && runghc Setup build \
+  && runghc Setup install )
+
+( cd regex-compat-0.95.1/ \
+  && runghc Setup configure --prefix="${GHC_TOP_DIR}" \
+  && runghc Setup build \
+  && runghc Setup install )
+
+make clean
+make boot
+make 2>&1 | tee ${LOG_NORMAL}
+
+make clean
+make boot
+make EXTRA_HC_OPTS="-XStrictData -DSTRICT_DATA" 2>&1 | tee ${LOG_STRICT}
+
+nofib-analyse/nofib-analyse ${LOG_NORMAL} ${LOG_STRICT} > ${COMP_NORMAL_STRICT}
