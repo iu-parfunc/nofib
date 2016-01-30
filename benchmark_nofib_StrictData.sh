d79e51005b15a12d0af3805b0f65e77352dad967
Parfunc Account <parfunc@googlegroups.com>
2016-01-29 19:25:29 -0500

    Copy over results to somewhere reliable

--- benchmark_nofib_StrictData.sh
+++ benchmark_nofib_StrictData.sh
@@ -4,12 +4,13 @@ set -xe
 
 WORKDIR=`pwd`
 GHC_TOP_DIR="${HOME}"/opt/ghc-8.0.0-20160111
+LOG_DIR="${HOME}"/nofib-results
 GHC_BIN_DIR="${GHC_TOP_DIR}"/bin
 
-LOG_NORMAL=nofib-log-normal
-LOG_STRICT=nofib-log-strict
+LOG_NORMAL=nofib-log-normal-$(date -d "today" +"%Y%m%d%H%M").log
+LOG_STRICT=nofib-log-strict-$(date -d "today" +"%Y%m%d%H%M").log
 
-COMP_NORMAL_STRICT=nofib-comp-normal-strict
+COMP_NORMAL_STRICT=nofib-comp-normal-strict-$(date -d "today" +"%Y%m%d%H%M").log
 
 export PATH=${GHC_BIN_DIR}:$PATH
 
@@ -47,3 +48,7 @@ make boot
 make EXTRA_HC_OPTS="-XStrictData -DSTRICT_DATA" 2>&1 | tee ${LOG_STRICT}
 
 nofib-analyse/nofib-analyse ${LOG_NORMAL} ${LOG_STRICT} > ${COMP_NORMAL_STRICT}
+
+cp ${LOG_NORMAL}         "${LOG_DIR}"/
+cp ${LOG_STRICT}         "${LOG_DIR}"/
+cp ${COMP_NORMAL_STRICT} "${LOG_DIR}"/
