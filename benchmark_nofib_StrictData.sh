9dda08a2bdda3a0beb07b4a4fb13b6a624894954
Parfunc Account <parfunc@googlegroups.com>
2016-01-30 19:13:44 -0500

    Attempt to use unboxed sums

--- benchmark_nofib_StrictData.sh
+++ benchmark_nofib_StrictData.sh
@@ -2,15 +2,19 @@
 
 set -xe
 
-WORKDIR=`pwd`
-GHC_TOP_DIR="${HOME}"/opt/ghc-8.0.0-20160111
+GHC_TOP_DIR="${HOME}"/opt/ghc-unboxed-sums-0.1
 LOG_DIR="${HOME}"/results_backup
 GHC_BIN_DIR="${GHC_TOP_DIR}"/bin
-
 LOG_EXT=.lgfile
+
 LOG_NORMAL=nofib-log-normal-$(date -d "today" +"%Y%m%d%H%M")${LOG_EXT}
 LOG_STRICT=nofib-log-strict-$(date -d "today" +"%Y%m%d%H%M")${LOG_EXT}
+LOG_UBXSUMS=nofib-log-ubxsums-$(date -d "today" +"%Y%m%d%H%M")${LOG_EXT}
 COMP_NORMAL_STRICT=nofib-comp-normal-strict-$(date -d "today" +"%Y%m%d%H%M")${LOG_EXT}
+COMP_NORMAL_UBXSUMS=nofib-comp-normal-ubxsums-$(date -d "today" +"%Y%m%d%H%M")${LOG_EXT}
+
+STRICT_FLAGS="-XStrictData -DSTRICT_DATA"
+UBXSUMS_FLAGS="${STRICT_FLAGS} -funbox-small-strict-sums=2"
 
 export PATH=${GHC_BIN_DIR}:$PATH
 
@@ -44,10 +48,17 @@ make 2>&1 | tee ${LOG_NORMAL}
 
 make clean
 make boot
-make EXTRA_HC_OPTS="-XStrictData -DSTRICT_DATA" 2>&1 | tee ${LOG_STRICT}
+make EXTRA_HC_OPTS="${STRICT_FLAGS}" 2>&1 | tee ${LOG_STRICT}
 
-nofib-analyse/nofib-analyse ${LOG_NORMAL} ${LOG_STRICT} > ${COMP_NORMAL_STRICT}
+make clean
+make boot
+make EXTRA_HC_OPTS="${UBXSUMS_FLAGS}" 2>&1 | tee ${LOG_UBXSUMS}
 
-cp ${LOG_NORMAL}         "${LOG_DIR}"/
-cp ${LOG_STRICT}         "${LOG_DIR}"/
-cp ${COMP_NORMAL_STRICT} "${LOG_DIR}"/
+nofib-analyse/nofib-analyse ${LOG_NORMAL} ${LOG_STRICT}  > ${COMP_NORMAL_STRICT}
+nofib-analyse/nofib-analyse ${LOG_NORMAL} ${LOG_UBXSUMS} > ${COMP_NORMAL_UBXSUMS}
+
+cp ${LOG_NORMAL}          "${LOG_DIR}"/
+cp ${LOG_STRICT}          "${LOG_DIR}"/
+cp ${LOG_UBXSUMS}         "${LOG_DIR}"/
+cp ${COMP_NORMAL_STRICT}  "${LOG_DIR}"/
+cp ${COMP_NORMAL_UBXSUMS} "${LOG_DIR}"/
